170bc5e97f5361a3eda7ec6afc78c61cfdef66cc
haileyhansard <haileyhansard@gmail.com>
2020-08-05 13:06:19 -0700

    Added GET requests for post id and post id with comment, and PUT request for add new post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -36,7 +36,68 @@ router.get('/:id', (req, res) => {
         res.status(500).json({ error: "The post information could not be retrieved." })
     })
 })
- 
+
+//GET	/api/posts/:id/comments
+//Returns an array of all the comment objects associated with the post with the specified id.
+router.get('/:id/comments', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        console.log(post)
+        if(!post[0]) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            Posts.findPostComments(req.params.id)
+            .then(comments => {
+                if (!comments[0]) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                } else {
+                    res.status(200).json(comments)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({ error: "The comments information could not be retrieved." })
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    })
+});
+
+//POST	/api/posts 
+//Creates a post using the information sent inside the request body.
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+    .then(postid => {
+        console.log(postid)
+        Posts.findById(postid.id)
+            .then(newPost => {
+                res.status(201).json(newPost)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+    .catch(err => {
+        console.log(err)
+        if (!req.body.title || !req.body.contents) {
+            return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        } else {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        }
+    })
+})
+
+//POST	/api/posts/:id/comments	
+//Creates a comment for the post with the specified id using information sent inside of the request body.
+
+//PUT	/api/posts/:id	
+//Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+
+//DELETE	/api/posts/:id	
+//Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
+
 module.exports = router;
 
 //this is like default export router 
