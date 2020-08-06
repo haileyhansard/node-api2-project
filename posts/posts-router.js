3da8d7a6ed69b1d521e19e59dea507ccdccb1cb3
haileyhansard <haileyhansard@gmail.com>
2020-08-05 20:06:07 -0700

    Added POST request for new comment to id specific post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const Posts = require('../data/db.js');
-const { remove } = require('../data/db.js');
 
 const router = express.Router();
 
@@ -12,7 +11,7 @@ router.get('/', (req, res) => {
     Posts.find(req.query)
         .then(posts => {
             console.log(posts)
-            res.status(200).json({ query: req.query, data: posts })
+            res.status(200).json({ data: posts })
         })
         .catch(err => {
         console.log(err)
@@ -92,9 +91,115 @@ router.post('/', (req, res) => {
 
 //POST	/api/posts/:id/comments	
 //Creates a comment for the post with the specified id using information sent inside of the request body.
-router.post('/:id/comments', (req, res) => {
-    Posts.insert()
+// router.post('/:id/comments', (req, res) => {
+//     const information = {...req.body, post_id: req.params.id}
+//     Posts.insertComment(information)
+//     .then(commentId => {
+//         console.log(commentId)
+//         Posts.findCommentById(commentId.id)
+//         .then(comment => {
+//             res.status(201).json(comment)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+//     })
+//     .catch(err => {
+//         console.log(err)
+//         Posts.findById(req.params.id)
+//         .then(post => {
+//             console.log(post)
+//             if(!post[0]){
+//                 res.status(404).json({ message: "The post with the specified ID does not exist." })
+//             } else if (!req.body.text) {
+//                res.status(400).json({ errorMessage: "Please provide text for the comment." })
+//             } else {
+//                 res.status(500).json({ error: "There was an error while saving the comment to the database" })
+//             }
+
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+//     })
+// });
+
+router.post('/:id/comments', (req, res) => { 
+    if(!req.body.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    } else if (req.body.text) {
+        console.log(req.params.id)
+        Posts.findById(req.params.id)
+            .then(post => {
+                if (post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+                } else {
+                Posts.insertComment(req.body)
+                    .then(comment => {
+                        res.status(201).json(comment);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        res.status(500).json({error: "There was an error while saving the comment to the database" });
+                    });
+                }
+            })
+            .catch(error => {
+            console.log(error)
+            res.status(500).json({ error: "There was an error while saving the comment to the database" })
+            })
+    };
 });
+
+    // Posts.findById(req.params.id)
+    //     .then(comment => console.log(comment))
+        // {
+        //     if(req.body.text) {
+        //         Posts.insertComment(req.body.text)
+        //         .then(comment => res.status(201).json(comment))
+        //         .catch(err => {
+        //             console.log(err)
+        //             res.status(500).json({ error: "The comments information could not be retrieved."})
+        //         })
+        //     } else {
+        //         res.status(400).json({errorMessage: "Please provide text for the comment." })
+        //     }
+        // })
+//         .catch(res.status(404).json({ message: "The post with the specified ID does not exist." }))
+// });
+
+//MARYAM'S CODE BELOW FROM TL GROUP
+// router.post("/:id/comments", (req, res) => {
+//     if (!req.body.text) {
+//       res.status(400).json({ errorMessage: "Please provide text for the comment." });
+//     } else if (req.body.text) {
+//       Posts.findById(req.params.id)
+//       .then(post => {
+//         if (post.length === 0) {
+//           res.status(404).json({ message: "The post with the specified ID does not exist." })
+//         } else {
+//           Posts.insertComment(req.body)
+//             .then(comment => {
+//               res.status(201).json(comment);
+//             })
+//             .catch(error => {
+//               console.log(error);
+//               res.status(500).json({error: "There was an error while saving the comment to the database" });
+//             });
+//         }
+//       })
+//       .catch(error => {
+//         console.log(error);
+//         res.status(500).json({ error: "The post information could not be retrieved." })
+//       })
+//     } 
+// });
+
+
+
+
+
+
 
 //PUT	/api/posts/:id	
 //Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
@@ -140,9 +245,9 @@ router.delete('/:id', (req, res) => {
         console.log(err)
         res.status(500).json({ error: "The post could not be removed" })
     })
-})
+});
 
 module.exports = router;
 
 //this is like default export router 
-//exports the component so its available on app
+//exports the component so its available on;
